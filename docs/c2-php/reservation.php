19d226fdc07fd9e93b4f99c298900e267c32cdf7
pedroslrn <pedrovslrn@gmail.com>
2024-06-17 16:16:11 -0300

    intativa de integracao MVC

---
+++ docs/c2-php/reservation.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+$myemail = 'you@yoursite.com';
+
+$date    = $_POST['date'];
+$email   = $_POST['email'];
+$subject = $_POST['subject'];
+$time    = $_POST['time'];
+$persons = $_POST['persons'];
+
+$to            = $myemail;
+$email_subject = "Table reservation";
+$email_body    = "\n Date: $date \n Email: $email \n Subject: Table reservation \n Time: $time \n Persons: $persons";
+$headers       = "From: $email";
+ 
+mail($to, $email_subject, $email_body, $headers);
+
+?>
